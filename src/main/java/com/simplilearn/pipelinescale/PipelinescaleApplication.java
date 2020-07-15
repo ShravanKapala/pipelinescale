6896a3f9d84553d66c4edda7fe2033599c9e7225
student <kapala.sravan@simplilearn.com>
2020-07-15 11:58:27 +0000

    Files added

---
+++ src/main/java/com/simplilearn/pipelinescale/PipelinescaleApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.pipelinescale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinescaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinescaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/pipelinescale/PipelinescaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.pipelinescale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinescaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
